27c4200de11305420dcb1a9c2c25f7de9e2cfe23
Nauman Ishaq <nauman.ishaq@brainxtech.com>
2025-05-26 20:24:47 +0500

    added index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//constants
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+
+//DB connection
+
+const connectDb = require('./connections/connect')
+connectDb();
+
+// JSON parser
+app.use(express.json());
+//import routers
+
+
+
+//setup routes
+app.use('/api/v1', require('./routes/index'))
+
+
+
+//listener
+app.listen(port, ()=>{console.log('listening on server 8000')});
